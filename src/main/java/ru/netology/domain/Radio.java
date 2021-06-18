6c72e5d4aaa3674bdbbade58ff6c304917de8b7f
Ekaterina Kolesnikova <1005885@mail.ru>
2021-06-18 20:04:45 +0300

    added file

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,117 @@
+package ru.netology.domain;
+
+public class Radio<numberRadioStation> {
+    private String name;
+    private int currentRadioStation;
+    private int maxRadioStation = 9;
+    private int minRadioStation = 0;
+    private int currentVolume;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private boolean on;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinRadioStation() {
+        return minRadioStation;
+    }
+
+    //    public void setMinRadioStation(int minRadioStation) {
+//        this.minRadioStation = minRadioStation;
+//    }
+
+    public int getMaxRadioStation() {
+        return maxRadioStation;
+    }
+
+//    public void setMaxRadioStation(int maxRadioStation) {
+//        this.maxRadioStation = maxRadioStation;
+//    }
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    //  Выбор текущей радиостанции.
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > maxRadioStation) {
+            return;
+        }
+        if (currentRadioStation < minRadioStation) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    //  Кнопка "Next";
+    public void nextButton() {
+        if (currentRadioStation == maxRadioStation) {
+            this.currentRadioStation = minRadioStation;
+            return;
+        }
+        this.currentRadioStation++;
+    }
+
+    //  Кнопка "Prev";
+    public void prevButton() {
+        if (currentRadioStation == minRadioStation) {
+            this.currentRadioStation = maxRadioStation;
+            return;
+        }
+        this.currentRadioStation--;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+//    public void setMinVolume(int minVolume) {
+//        this.minVolume = minVolume;
+//    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+//    public void setMaxVolume(int maxVolume) {
+//        this.maxVolume = maxVolume;
+//    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    //    Кнопка "Плюс" (Уровень громкости);
+    public void plusButton() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        this.currentVolume++;
+    }
+
+    //    Кнопка "Минус" (Уровень громкости);
+    public void minusButton() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        this.currentVolume--;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+//    public void setOn(boolean on) {
+//        this.on = on;
+//    }
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,166 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+
+    Radio radio = new Radio();
+
+    @Test
+    public void shouldGetAndSetNameRadio() {
+
+        String expected = "Радио - Мир";
+
+        assertNull(radio.getName());
+        radio.setName(expected);
+        assertEquals(expected, radio.getName());
+    }
+
+    @Test
+    public void shouldGetAndSet() {
+        assertEquals(0, radio.getMinRadioStation());
+        assertEquals(9, radio.getMaxRadioStation());
+        assertEquals(0, radio.getMinVolume());
+        assertEquals(10, radio.getMaxVolume());
+
+    }
+
+    //  Текущая радиостанция в допустимом диапазоне;
+    @Test
+    public void currentRadioStation() {
+
+        radio.setCurrentRadioStation(8);
+
+        int expected = 8;
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    //  Текущая радиостанция выше граничного значения;
+    @Test
+    public void currentRadioStationAbovelimitValue() {
+
+        radio.setCurrentRadioStation(10);
+
+        int expected = 0;
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    //  Текущая радиостанция ниже граничного значения;
+    @Test
+    public void currentRadioStationBelowlimitValue() {
+
+        radio.setCurrentRadioStation(-1);
+
+        int expected = 0;
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    //  Кнопка "Next".
+    public void nextStationButton() {
+
+        radio.nextButton();
+
+        int expected = 1;
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    //  Кнопка "Prev".
+    public void prevStationButton() {
+
+        radio.setCurrentRadioStation(8);
+        radio.prevButton();
+
+        int expected = 7;
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    //  Следующая радиостанция выше максимальной;
+    public void nextStationButtonAboveMaxRadioStation() {
+
+        radio.setCurrentRadioStation(9);
+        radio.nextButton();
+
+        int expected = 0;
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    //  Предыдущая радиостанция ниже минимальной;
+    public void prevStationButtonBelowMinRadioStation() {
+
+        radio.setCurrentRadioStation(-1);
+        radio.prevButton();
+
+        int expected = 9;
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    //  Текущий уровень громкости выше граничного значения;
+    @Test
+    void currentVolumeAbovelimitValue() {
+
+        radio.setCurrentVolume(10);
+        radio.plusButton();
+
+        int expected = 10;
+        int actual = radio.getCurrentVolume();
+        assertEquals(expected, actual);
+    }
+
+    //  Текущий уровень громкости ниже граничного значения;
+    @Test
+    void setCurrentVolumeBelowlimitValue() {
+
+        radio.minusButton();
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        assertEquals(expected, actual);
+    }
+
+    //    Переключение громкости звука вперед +1.
+    @Test
+    public void shouldSwitchVolumeForward() {
+
+        radio.setCurrentVolume(4);
+        radio.plusButton();
+
+        int expected = 5;
+        int actual = radio.getCurrentVolume();
+        assertEquals(expected, actual);
+    }
+
+    //    Переключение громкости звука назад -1.
+    @Test
+    public void shouldSwitchVolumeBack() {
+
+        radio.setCurrentVolume(4);
+        radio.minusButton();
+
+        int expected = 3;
+        int actual = radio.getCurrentVolume();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRadioIsOn() {
+
+        radio.isOn();
+
+        assertFalse(radio.isOn());
+
+    }
+
+}
